4845a0f83e5e2e64b00c5ee801a6f25d322477fc
Maja Stefanowicz <38382398+MajaSt1@users.noreply.github.com>
2024-07-09 09:13:25 +0000

    add todo functionality

--- main.go
+++ main.go
@@ -7,17 +7,32 @@ import (
 	"strings"
 
 	"example.com/note/note"
+	"example.com/note/todo"
 )
 
 func main() {
 	title, content := getNoteData()
+	todoText := getUserInput("Todo text: ")
 
-	userNote, err := note.New(title, content)
-
+	todo, err := todo.New(todoText)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
+
+	userNote, err := note.New(title, content)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	todo.Display()
+	err = todo.Save()
+	if err != nil {
+		fmt.Println("Saving the todo failed.")
+		return
+	}
+	fmt.Println("Saving the todo succeeded!")
 
 	userNote.Display()
 	err = userNote.Save()
