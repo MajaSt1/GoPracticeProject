2f3786a099800811689e41ff90b870a6d54d8a13
Maja Stefanowicz <38382398+MajaSt1@users.noreply.github.com>
2024-07-10 07:59:41 +0000

    refactor code for next module

---
+++ interfacesgeneric/example.go
@@ -0,0 +1,125 @@
+package interfacesgeneric
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+
+	"example.com/note/note"
+	"example.com/note/todo"
+)
+
+type saver interface { //one method + `er` convention
+	Save() error
+}
+
+type outputtable interface { // embedded interfaces
+	saver
+	Display()
+}
+
+func ShowExample() {
+	title, content := getNoteData()
+	todoText := getUserInput("Todo text: ")
+
+	todo, err := todo.New(todoText)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	userNote, err := note.New(title, content)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	err = outputData(todo) // don't have to connect interface explicitly to some type (instead must me the same method in this type)!
+	if err != nil {
+		return
+	}
+
+	outputData(userNote)
+}
+
+func printSomething(value interface{}) {
+	intVal, ok := value.(int)
+	if ok {
+		fmt.Println("Integer:", intVal)
+		return
+	}
+
+	floatVal, ok := value.(float64)
+	if ok {
+		fmt.Println("Float:", floatVal)
+		return
+	}
+
+	stringVal, ok := value.(string)
+	if ok {
+		fmt.Println("String:", stringVal)
+		return
+	}
+
+	// switch value.(type) {
+	// case int:
+	// 	fmt.Println("Integer:", value)
+	// case float64:
+	// 	fmt.Println("Float:", value)
+	// case string:
+	// 	fmt.Println(value)
+	// }
+}
+
+func outputData(data outputtable) error {
+	data.Display()
+	return saveData(data)
+}
+
+func saveData(data saver) error {
+	err := data.Save()
+	if err != nil {
+		fmt.Println("Saving the note failed.")
+		return err
+	}
+	fmt.Println("Saving the note succeeded!")
+	return nil
+}
+
+func getNoteData() (string, string) {
+	title := getUserInput("Note title:")
+	content := getUserInput("Note content:")
+
+	return title, content
+}
+
+func getUserInput(prompt string) string {
+	fmt.Printf("%v ", prompt)
+
+	reader := bufio.NewReader(os.Stdin)
+
+	text, err := reader.ReadString('\n')
+
+	if err != nil {
+		return ""
+	}
+
+	text = strings.TrimSuffix(text, "\n")
+	text = strings.TrimSuffix(text, "\r")
+
+	return text
+}
+
+// func add[T any](a, b T) T { //any = interface{}
+// 	return a + b
+// 	// asInt, aIsInt := a.(int)
+// 	// bInt, bIsInt := b.(int)
+// 	// if aIsInt && bIsInt {
+// 	// 	return asInt + bInt
+// 	// }
+// }
+
+func add[T int | float64 | string](a, b T) T {
+	return a + b
+}
