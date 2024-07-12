4a1768372b86326e271ec71d4c4c5ef59c728712
Maja Stefanowicz <38382398+MajaSt1@users.noreply.github.com>
2024-07-12 07:07:28 +0000

    returning functions as values

--- functions/functions.go
+++ functions/functions.go
@@ -6,15 +6,24 @@ import "fmt"
 type transformFn func(int) int
 
 // map where the key is string and values are slices of integers
-type anotherFn func(int, []string, map[string][]int) ([]int, string)
+// type anotherFn func(int, []string, map[string][]int) ([]int, string)
 
 func ShowExample() {
 	numbers := []int{1, 2, 3, 4}
 	// functions are just values in go
+	moreNumbers := []int{5, 1, 2}
 	doubled := transformNumbers(&numbers, double)
 	tripled := transformNumbers(&numbers, triple)
 
 	fmt.Println(doubled, tripled)
+
+	transformFn1 := getTransformerFn(&numbers)
+	transformFn2 := getTransformerFn(&moreNumbers)
+
+	transformedNumbers := transformNumbers(&numbers, transformFn1)
+	moreTransformedNumbers := transformNumbers(&moreNumbers, transformFn2)
+
+	fmt.Println(transformedNumbers, moreTransformedNumbers)
 }
 
 // pointer to slice of integers
@@ -33,4 +42,12 @@ func double(number int) int {
 
 func triple(number int) int {
 	return number * 3
+}
+
+func getTransformerFn(numbers *[]int) transformFn {
+	if (*numbers)[0] == 1 {
+		return double
+	} else {
+		return triple
+	}
 }
