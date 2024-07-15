0d86f5aaa2d2a8b7aafcb1ad5663b747343fd798
Maja Stefanowicz <38382398+MajaSt1@users.noreply.github.com>
2024-07-15 16:37:13 +0000

    deadlock example

--- concurrency/goroutines.go
+++ concurrency/goroutines.go
@@ -22,17 +22,22 @@ func slowGreet(phrase string, doneChan chan bool) {
 // dispatch(doesn't wait for function/goroutine to complete) these four goroutines/tasks and we're done/exit from main function = dispatching goroutines is faster than execution/complete these functions
 // SOLUTION: channels - path to communicate with goroutines
 func ShowGreetExample() {
+	dones := make([]chan bool, 4)
 	// You can use one and the same channel with multiple Goroutines because this channel is in the end a transmission device. Its capable to be used to receive multiple values.
-	done := make(chan bool)
+	// done := make(chan bool)
 
-	go greet("Nice to meet you!", done)
-	go greet("How are you?", done)
-	go slowGreet("How ... are ... you ...?", done)
-	go greet("I hope you're liking the course!", done)
+	dones[0] = make(chan bool)
+	go greet("Nice to meet you!", dones[0])
+	go greet("How are you?", dones[1])
+	go slowGreet("How ... are ... you ...?", dones[2])
+	go greet("I hope you're liking the course!", dones[3])
 
+	for _, done := range dones {
+		<-done
+	}
 	// you have to wait for as many values as you have goroutines when you want them to be finished
-	<-done
-	<-done
-	<-done
-	<-done //we waiting for data to come out of the channel and sending it to a void - to go simply means that we're done as soon as we got one value out of this channel.(race condition)
+	// <-done
+	// <-done
+	// <-done
+	// <-done //we waiting for data to come out of the channel and sending it to a void - to go simply means that we're done as soon as we got one value out of this channel.(race condition)
 }
