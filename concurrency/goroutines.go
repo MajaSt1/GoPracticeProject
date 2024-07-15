59b86fe875c9377e8353e611ad1d059893f78fe7
Maja Stefanowicz <38382398+MajaSt1@users.noreply.github.com>
2024-07-15 16:42:54 +0000

    using slices when using multiple channels with goroutines

--- concurrency/goroutines.go
+++ concurrency/goroutines.go
@@ -28,8 +28,11 @@ func ShowGreetExample() {
 
 	dones[0] = make(chan bool)
 	go greet("Nice to meet you!", dones[0])
+	dones[1] = make(chan bool)
 	go greet("How are you?", dones[1])
+	dones[2] = make(chan bool)
 	go slowGreet("How ... are ... you ...?", dones[2])
+	dones[3] = make(chan bool)
 	go greet("I hope you're liking the course!", dones[3])
 
 	for _, done := range dones {
