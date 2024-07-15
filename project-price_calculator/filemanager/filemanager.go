73eed493046c71caa8388e70be8b40a613acc9f5
Maja Stefanowicz <38382398+MajaSt1@users.noreply.github.com>
2024-07-15 12:33:07 +0000

    storing JSON data in files

--- project-price_calculator/filemanager/filemanager.go
+++ project-price_calculator/filemanager/filemanager.go
@@ -2,6 +2,7 @@ package filemanager
 
 import (
 	"bufio"
+	"encoding/json"
 	"errors"
 	"os"
 )
@@ -27,4 +28,18 @@ func Readlines(path string) ([]string, error) {
 
 	file.Close()
 	return lines, nil
+}
+
+func WriteJSON(path string, data interface{}) error {
+	file, err := os.Create(path)
+	if err != nil {
+		return errors.New("Failed to create file.")
+	}
+	encoder := json.NewEncoder(file)
+	err = encoder.Encode(data)
+	if err != nil {
+		return errors.New("Failed to convert data to JSON.")
+	}
+	file.Close()
+	return nil
 }
--- project-price_calculator/price_calculator.go
+++ project-price_calculator/price_calculator.go
@@ -1,23 +1,18 @@
 package projectpricecalculator
 
 import (
+	"fmt"
+
 	"example.com/note/project-price_calculator/conversion"
 	"example.com/note/project-price_calculator/filemanager"
-	"fmt"
 )
 
 func ShowPriceCalculator() {
 	taxRates := []float64{0, 0.07, 0.1, 0.15}
 	for _, taxRate := range taxRates {
 		priceJob := NewTaxIncludedPriceJob(taxRate)
-		priceJob.Process()
+		priceJob.process()
 	}
-}
-
-type TaxIncludedPriceJob struct {
-	TaxRate           float64
-	InputPrices       []float64
-	TaxIncludedPrices map[string]float64
 }
 
 func (job *TaxIncludedPriceJob) loadData() {
@@ -37,7 +32,7 @@ func (job *TaxIncludedPriceJob) loadData() {
 	job.InputPrices = prices
 }
 
-func (job *TaxIncludedPriceJob) Process() {
+func (job *TaxIncludedPriceJob) process() {
 	job.loadData()
 	result := make(map[string]string)
 	for _, price := range job.InputPrices {
@@ -45,7 +40,15 @@ func (job *TaxIncludedPriceJob) Process() {
 		result[fmt.Sprintf("%.2f", price)] = fmt.Sprintf("%.2f", taxIncludedPrice)
 	}
 
-	fmt.Println(result)
+	job.TaxIncludedPrices = result
+
+	filemanager.WriteJSON(fmt.Sprintf("result_%.0f.json", job.TaxRate*100), job)
+}
+
+type TaxIncludedPriceJob struct {
+	TaxRate           float64
+	InputPrices       []float64
+	TaxIncludedPrices map[string]string
 }
 
 func NewTaxIncludedPriceJob(taxRate float64) *TaxIncludedPriceJob { // create this value only once in memory and we share address to that value
