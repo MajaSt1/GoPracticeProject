e3f1defd6906f8b4b605863a905297d6ff5def4c
Maja Stefanowicz <38382398+MajaSt1@users.noreply.github.com>
2024-07-15 16:29:50 +0000

    race condition when using the same channel example

--- concurrency/goroutines.go
+++ concurrency/goroutines.go
@@ -7,24 +7,27 @@ import (
 	"time"
 )
 
-func greet(phrase string) {
+func greet(phrase string, doneChan chan bool) {
 	fmt.Println("Hello!", phrase)
+	doneChan <- true
 }
 
 func slowGreet(phrase string, doneChan chan bool) {
 	time.Sleep(3 * time.Second) // simulate a slow, long-taking task
 	fmt.Println("Hello!", phrase)
-	doneChan <- true //<- sends data(true) to a channel
+	doneChan <- true //<- sends data(true) through a channel when its done
 }
 
 // go - meaing running in parallel as goroutines - running in non blocking way
 // dispatch(doesn't wait for function/goroutine to complete) these four goroutines/tasks and we're done/exit from main function = dispatching goroutines is faster than execution/complete these functions
 // SOLUTION: channels - path to communicate with goroutines
 func ShowGreetExample() {
-	// go greet("Nice to meet you!")
-	// go greet("How are you?")
+	// You can use one and the same channel with multiple Goroutines because this channel is in the end a transmission device. Its capable to be used to receive multiple values.
 	done := make(chan bool)
+
+	go greet("Nice to meet you!", done)
+	go greet("How are you?", done)
 	go slowGreet("How ... are ... you ...?", done)
-	// go greet("I hope you're liking the course!")
+	go greet("I hope you're liking the course!", done)
 	<-done //we waiting for data to come out of the channel and sending it to a void
 }
