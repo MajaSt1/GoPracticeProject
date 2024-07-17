9452b0bd6695c99dd07ff7c696e9580499d5201a
Maja Stefanowicz <38382398+MajaSt1@users.noreply.github.com>
2024-07-17 12:18:50 +0000

    getting events from database (SELECT), Storing Data (INSERT), exec/query vs prepare note

--- rest-api-project/models/event.go
+++ rest-api-project/models/event.go
@@ -1,9 +1,13 @@
 package models
 
-import "time"
+import (
+	"time"
+
+	"example.com/note/rest-api-project/db"
+)
 
 type Event struct {
-	ID          int
+	ID          int64
 	Name        string    `binding:"required"`
 	Description string    `binding:"required"`
 	Location    string    `binding:"required"`
@@ -13,11 +17,42 @@ type Event struct {
 
 var events = []Event{}
 
-func (e Event) Save() {
-	// later: add it to a database
-	events = append(events, e)
+func (e Event) Save() error {
+	query := `
+	INSERT INTO events(name, description, location, dateTime, user_id) 
+	VALUES (?, ?, ?, ?, ?)`
+	stmt, err := db.DB.Prepare(query) // stored in memory by the sql package and reuse it with different values
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+
+	result, err := stmt.Exec(e.Name, e.Description, e.Location, e.DateTime, e.UserID) // Exec - we use when we want to make changes in db /insert
+	if err != nil {
+		return err
+	}
+	id, err := result.LastInsertId() // get event.ID
+	e.ID = id
+	return err
 }
 
-func GetAllEvents() []Event {
-	return events
+func GetAllEvents() ([]Event, error) {
+	query := "SELECT * FROM events"
+	rows, err := db.DB.Query(query) // fetch data
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var events []Event
+	for rows.Next() {
+		var event Event
+		err := rows.Scan(&event.ID, &event.Name, &event.Location, &event.DateTime, &event.UserID)
+		if err != nil {
+			return nil, err
+		}
+
+		events = append(events, event)
+	}
+	return events, nil
 }
