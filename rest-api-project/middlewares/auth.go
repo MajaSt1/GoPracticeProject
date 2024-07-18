118b6f7f0a781ba5c87c4bd5066b5ea0101a3829
Maja Stefanowicz <38382398+MajaSt1@users.noreply.github.com>
2024-07-18 13:38:00 +0000

    adding an auth middleware

---
+++ rest-api-project/middlewares/auth.go
@@ -0,0 +1,24 @@
+package middlewares
+
+import (
+	"net/http"
+
+	"example.com/note/rest-api-project/utils"
+	"github.com/gin-gonic/gin"
+)
+
+func Authenticate(ctx *gin.Context) {
+	token := ctx.Request.Header.Get("Authorization")
+	if token == "" {
+		ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"message": "Not authorized."})
+		return
+	}
+
+	userId, err := utils.VerifyToken(token)
+	if err != nil {
+		ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"message": "Not authorized."})
+		return
+	}
+
+	ctx.Next()
+}
