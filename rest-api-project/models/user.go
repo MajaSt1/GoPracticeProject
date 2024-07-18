219179d744feb2676f045316ee644e783449f5c0
Maja Stefanowicz <38382398+MajaSt1@users.noreply.github.com>
2024-07-18 11:44:05 +0000

    login route

--- rest-api-project/models/user.go
+++ rest-api-project/models/user.go
@@ -1,6 +1,11 @@
 package models
 
-import "example.com/note/rest-api-project/db"
+import (
+	"errors"
+
+	"example.com/note/rest-api-project/db"
+	"example.com/note/rest-api-project/utils"
+)
 
 type User struct {
 	ID int64
@@ -17,7 +22,12 @@ func (u User) Save() error {
 
 	defer stmt.Close()
 
-	result, err := stmt.Exec(u.Email, u.Password)
+	hashedPasswored, err := utils.HashPassword(u.Password)
+	if err != nil {
+		return err
+	}
+
+	result, err := stmt.Exec(u.Email, hashedPasswored)
 	if err != nil {
 		return err
 	}
@@ -25,4 +35,21 @@ func (u User) Save() error {
 	userId, err := result.LastInsertId()
 	u.ID = userId
 	return err // nil / error
+}
+
+func (u User) ValidateCredentials() error {
+	query := "SELECT email, password FROM users WHERE email = ?"
+	row := db.DB.QueryRow(query, u.Email) // we know that we query exactly one row
+	var retrievedPwd string
+	err := row.Scan(&retrievedPwd)
+	if err != nil {
+		return errors.New("Credentials invalid.")
+	}
+	
+	passwotdIsValid := utils.CheckPasswordHash(u.Password, retrievedPwd)
+	if !passwotdIsValid {
+		return errors.New("Credentials invalid.")
+	}
+
+	return nil
 }
--- rest-api-project/routes/users.go
+++ rest-api-project/routes/users.go
@@ -23,3 +23,20 @@ func signup(ctx *gin.Context){
 
 	ctx.JSON(http.StatusCreated, gin.H{"message": "User created!", "user": user})
 }
+
+func login(ctx *gin.Context){
+	var user models.User
+	err := ctx.ShouldBindJSON(&user)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse request data."})
+		return
+	}
+
+	err = user.ValidateCredentials()
+	if err != nil {
+		ctx.JSON(http.StatusUnauthorized, gin.H{"message": "Could not authenicate user."})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{"message": "Login successful!"})
+}
